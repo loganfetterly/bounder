587e739adac4f013d68615546408c5f8811a0240
Logan Fetterly <lfetterly25@gmail.com>
2020-08-17 13:00:51 -0600

    getBounds working, test files uploaded

--- app.py
+++ app.py
@@ -5,13 +5,52 @@
 
 
 # IMPORTS
+import re
 from geopy.geocoders import Nominatim
 
 # MAIN BLOCK
 
-geolocator = Nominatim(user_agent="app.py")
-location = geolocator.geocode("175 5th Avenue NYC")
-print(location.address)
-print((location.latitude, location.longitude))
-(40.7410861, -73.9896297241625)
-print(location.raw)
+class Bounder:
+
+    # Fields:
+    #     file - file object for reading
+    #     allAddresses - all addresses before determining within bounding
+    #     inBoxAddresses - all addresses within the bounds
+    #     boundingBox - coordinates for the bounding shape
+    #     geoCoder - geocoder for getting long and lat from addresses
+    #     lines - an array of string (lines) from the input file
+
+    def __init__(self, file):
+        self.file = open(file)
+        self.allAddresses = []
+        self.inBoxAddresses = []
+        self.boundingBox = []
+        self.geoCoder = Nominatim(user_agent="app.py")
+        self.lines = self.file.readlines()
+
+    # If in bounds append to inBoxAddresses
+    # def inBounds(self):
+
+    # Take file and get the bounding information inside the boundingBox array
+    # EXAMPLE INPUT (37.5, -122.5), (38.2, -121.6), (37.0, -121.4), (36.6, -121.3)
+
+    def getBounds(self):
+        coordinatePoints = re.findall(r"[+-]?\d+\.?\d*", self.lines[0])
+        for num in range(len(coordinatePoints)):
+            coordinatePoints[num] = float(coordinatePoints[num])
+        self.boundingBox = coordinatePoints
+
+    # Take all the strings of addresses and store them inside the addresses array
+    # def getAddresses(self):
+
+    
+# ------------------------------------------------------------
+# geolocator = Nominatim(user_agent="app.py")
+# location = geolocator.geocode("175 5th Avenue NYC")
+# print(location.address)
+# print((location.latitude, location.longitude))
+# (40.7410861, -73.9896297241625)
+# print(location.raw)
+
+x = Bounder("test.txt")
+x.getBounds()
