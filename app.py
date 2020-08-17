40a7dceb72ab1a7ab56efe9e17aa7f29e74c2839
Logan Fetterly <lfetterly25@gmail.com>
2020-08-17 16:08:39 -0600

    app.py working with command line

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from classes.Bounder import Bounder as bd
+import sys
+from classes.Bounder import Bounder as bd 
 
-x = bd("test.txt")
-x.printOutput()
+print()
+print("Running your script.. ", sys.argv[0])
+
+if (len(sys.argv) != 2):
+    print("Usage: python app.py <your_test_text_file>")
+else:
+    x = bd(sys.argv[1])
+    x.printOutput()
